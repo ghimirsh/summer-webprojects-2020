1ec14cfca41bc514c53f3a0a17bc942e41f9d3c8
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-04-28 19:57:37 -0400

    Add all navigation pages to metric conversion project

---
+++ metric-conversion/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metric-Conversion</title>
+</head>
+<body>
+    <div id="main-content">
+        <h1>Measurement Conversion</h1>
+        <ul id="measurement-types">
+            <li><a href="./pages/length.php"> Length and Distance</a></li>
+            <li><a href="./pages/area.php"> Area</a></li>
+            <li><a href="./pages/volume.php"> Volume and Capacity</a></li>
+            <li><a href="./pages/mass.php"> Mass and Weight</a></li>
+            <li><a href="./pages/speed.php">Speed </a></li>
+            <li><a href="./pages/temperature.php">Temperature</a></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ metric-conversion/pages/area.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metric Conversion</title>
+</head>
+<body>
+    <h1>Area Conversion</h1>
+
+    <p>
+        <a href="../index.php">Return to Homepage</a>
+    </p>
+</body>
+</html>
